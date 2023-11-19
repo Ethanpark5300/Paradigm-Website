e050d3b790199896d3381f9dbc2c97fd43371104
Ethan Park <ethanpark5300@gmail.com>
2023-11-19 03:14:20 -0700

    Attmept to fix back to top button

--- scripts/backtotopbutton.js
+++ scripts/backtotopbutton.js
@@ -1,18 +1,23 @@
-const navbarHeight = 90;
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
 
 document.addEventListener("scroll", () => {
-  if (scrollContainer().scrollTop > showOnPx + navbarHeight) {
-    backToTopButton.classList.remove("hidden");
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
   } else {
-    backToTopButton.classList.add("hidden");
+    backToTopButton.classList.add("hidden")
   }
-});
+})
 
 const goToTop = () => {
-  scrollContainer().scrollIntoView({
+  
+  document.body.scrollIntoView({
     behavior: "smooth",
-    block: "start",
   });
 };
 
-backToTopButton.addEventListener("click", goToTop);
+backToTopButton.addEventListener("click", goToTop)

7ab7a1b9a521fe1cd660d04019e6f80f4bea553f
Ethan Park <ethanpark5300@gmail.com>
2023-11-29 16:05:23 -0700

    Attempt fix back to top button

--- scripts/backtotopbutton.js
+++ scripts/backtotopbutton.js
@@ -1,23 +1,40 @@
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".back-to-top")
+// const showOnPx = 100;
+// const backToTopButton = document.querySelector(".back-to-top")
 
-const scrollContainer = () => {
-  return document.documentElement || document.body;
+// const scrollContainer = () => {
+//   return document.documentElement || document.body;
+// };
+
+// document.addEventListener("scroll", () => {
+//   if (scrollContainer().scrollTop > showOnPx) {
+//     backToTopButton.classList.remove("hidden")
+//   } else {
+//     backToTopButton.classList.add("hidden")
+//   }
+// })
+
+// const goToTop = () => {
+
+//   document.body.scrollIntoView({
+//     behavior: "smooth",
+//   });
+// };
+
+// backToTopButton.addEventListener("click", goToTop)
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Show/hide the back to top button based on scroll position
+window.onscroll = function () {
+    var backToTopBtn = document.getElementById("backToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
 };
-
-document.addEventListener("scroll", () => {
-  if (scrollContainer().scrollTop > showOnPx) {
-    backToTopButton.classList.remove("hidden")
-  } else {
-    backToTopButton.classList.add("hidden")
-  }
-})
-
-const goToTop = () => {
-  
-  document.body.scrollIntoView({
-    behavior: "smooth",
-  });
-};
-
-backToTopButton.addEventListener("click", goToTop)

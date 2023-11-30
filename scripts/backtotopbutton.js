c3ba3374a3d1dddae534bbb1b5b8a2763cf57e83
Ethan Park <ethanpark5300@gmail.com>
2023-11-29 22:45:38 -0700

    Test back to top button

--- scripts/backtotopbutton.js
+++ scripts/backtotopbutton.js
@@ -1,6 +1,14 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Show/hide the back to top button based on scroll position
 window.onscroll = function () {
     var backToTopBtn = document.getElementById("backToTopBtn");
-    if (window.pageYOffset > 20) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         backToTopBtn.style.display = "block";
     } else {
         backToTopBtn.style.display = "none";

194fb1ddc01b967217c28e77d2dc3f7f8406f133
Ethan Park <ethanpark5300@gmail.com>
2023-12-06 05:09:02 -0700

    Attempt contact form4

--- scripts/backbutton.js
+++ scripts/backbutton.js
@@ -1,10 +1,11 @@
 function goBack() {
+    console.log("Hello")
     const referrer = document.cookie.replace(/(?:(?:^|.*;\s*)referrer\s*=\s*([^;]*).*$)|^.*$/, "$1");
     if (referrer === "page1") {
-        window.location.href = "./index.html";
+        window.location.href = "../index.html";
     } 
     else if (referrer === "page2") {
-        window.location.href = "./contact.html";
+        window.location.href = "../pages/contact.html";
     }
     // } else {
     //     // Default behavior if referrer is not recognized

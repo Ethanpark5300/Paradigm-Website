dabdcb0d0ac34a76d06101cfbb8e82c92ba5013d
Ethan Park <ethanpark5300@gmail.com>
2023-12-06 19:02:32 -0700

    Form test

--- scripts/backbutton_contact.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function () {
-    document.cookie = "referrer=page2; expires=0; path=/";
-});
--- scripts/backbutton_home.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('home-form').addEventListener('submit', function () {
-    document.cookie = "referrer=page1; expires=0; path=/";
-});
